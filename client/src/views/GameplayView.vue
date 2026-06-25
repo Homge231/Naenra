ac790db8d84cb6637717be69f5cd049d994cf90d
Homge231 <homge231@gmail.com>
2026-06-25 16:21:02 +0700

    fix: points fix [AI 0%]

--- client/src/views/GameplayView.vue
+++ client/src/views/GameplayView.vue
@@ -179,7 +179,7 @@
         :style="{ width: `${(timeLeft / MATCH_DURATION) * 100}%` }"></div>
     </div>
 
-    <div v-if="playerAvatarUrl" class="absolute bottom-6 left-6 z-30 pointer-events-none">
+    <div v-if="playerAvatarUrl" class="absolute bottom-6 left-6 z-60 pointer-events-none">
       <div class="w-16 h-16 rounded-full bg-gradient-to-br from-blue to-lightBlue p-0.5 shadow-[0_0_15px_rgba(59,130,246,0.3)]">
         <img :src="playerAvatarUrl" alt="Player Avatar" class="w-full h-full rounded-full object-cover bg-darkNavy" />
       </div>
