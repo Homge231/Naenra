b4b4e48989db00322d7aaf4b8b4bb5ad1527bd3f
Homge231 <homge231@gmail.com>
2026-06-29 16:06:39 +0700

    feat: display question, update claude and readme [AI 0%]

--- client/src/components/Avatar.vue
+++ client/src/components/Avatar.vue
@@ -1,9 +1,9 @@
 <template>
   <div
     v-if="src"
-    class="fixed bottom-1 left-6 z-[60] pointer-events-none translate-y-1/2"
+    class="fixed top-24 left-8 lg:left-12 z-20 pointer-events-none"
   >
-    <div class="w-24 h-24 rounded-full bg-gradient-to-br from-blue to-lightBlue p-0.5 shadow-[0_0_15px_rgba(59,130,246,0.3)]">
+    <div class="w-14 h-14 rounded-full bg-gradient-to-br from-blue to-lightBlue p-0.5 shadow-[0_0_15px_rgba(59,130,246,0.3)]">
       <img
         :src="src"
         :alt="alt"
